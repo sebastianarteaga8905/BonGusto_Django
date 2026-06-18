bc1e3167067c0c16b9d3f043ab2ac6fec97438b1
sebastianarteaga8905 <sebastianarteaga8905@gmail.com>
2026-06-18 16:04:51 -0500

    Actualizacion

--- src/bongusto/infrastructure/settings.py
+++ src/bongusto/infrastructure/settings.py
@@ -335,6 +335,8 @@ EMAIL_PORT = int(os.getenv("EMAIL_PORT", "587"))
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "true").lower() == "true"
+EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", "false").lower() == "true"
+EMAIL_TIMEOUT = int(os.getenv("EMAIL_TIMEOUT", "20"))
 
 DEFAULT_FROM_EMAIL = os.getenv(
     "DEFAULT_FROM_EMAIL",
